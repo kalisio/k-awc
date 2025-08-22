d9ff150d53dfb17e9eeeccc9994e6bd50d242875
cnouguier <ch.nouguier@gmail.com>
2025-08-22 18:12:39 +0200

    wip: Provide a run_jobs script #13

--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# run stations job
 krawler ./jobfile.stations.js
+# run metars job
 export DATA=metars
 krawler ./jobfile.metars-tafs.js
+# run tafs job
 export DATA=tafs
 krawler ./jobfile.metars-tafs.js
